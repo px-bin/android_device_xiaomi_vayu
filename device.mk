e5370e2d57fc5a1257fda110838d635c849403ac
pedro <96991500+px-bin@users.noreply.github.com>
2022-03-03 13:05:12 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -28,6 +28,10 @@ PRODUCT_PACKAGES += \
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
